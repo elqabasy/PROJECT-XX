f6d4e17b75d1efc2ac5514f3cb4eed9871203a65
Mahros <mahros.elqabasy@hotmail.com>
2024-01-07 00:48:48 +0200

    Feature-Animation

--- headers/Constants.h
+++ headers/Constants.h
@@ -1,4 +1,16 @@
 #ifndef PROJECT_XX_CONSTANTS_H
 #define PROJECT_XX_CONSTANTS_H
-
+namespace Constants{
+    namespace Console{
+        enum DIRECTIONS{
+            UP = 72,
+            LEFT = 75,
+            RIGHT = 77,
+            DOWN = 80,
+            LEFT_TO_RIGHT = -1,
+            RIGHT_TO_LEFT = 1,
+            RIGHT_AND_LEFT = 0,
+        };
+    }
+} // namespace Constants
 #endif
--- sources/Console.cpp
+++ sources/Console.cpp
@@ -12,14 +12,40 @@ void Console::getConsoleSize(int& rows, int& cols) {
     cols = CSBI.srWindow.Right - CSBI.srWindow.Left + 1;
 }
 
-void Console::setCursor(const int& rowPos, const int& colPos) {
-    cout << "\x1b[" << rowPos << ";" << colPos << "H";
+void Console::setCursorPos(short int rowPos, short int colPos) {
+    // cout << "\x1b[" << rowPos << ";" << colPos << "H";
+    COORD pos = {colPos, rowPos};
+    HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(output, pos);
 }
 
 void Console::resetCursor(){
-    cout << "\x1b[" << 0 << ";" << 0 << "H";
+    // cout << "\x1b[" << 0 << ";" << 0 << "H";
+    COORD pos = {0, 0};
+    HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(output, pos);
 }
 
 void Console::sleep(const int& ms) {
+    // if
     this_thread::sleep_for(chrono::milliseconds{ms});
 }   
+
+void Console::getCursorPos(int &row, int &col){
+    CONSOLE_SCREEN_BUFFER_INFO CSBI;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &CSBI);
+    row = CSBI.dwCursorPosition.Y;
+    col = CSBI.dwCursorPosition.X;
+}
+
+
+void Console::getCusrorRowPos(int &row){
+    CONSOLE_SCREEN_BUFFER_INFO CSBI;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &CSBI);
+    row = CSBI.dwCursorPosition.Y;
+}
+void Console::getCusrorColPos(int &col){
+    CONSOLE_SCREEN_BUFFER_INFO CSBI;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &CSBI);
+    col = CSBI.dwCursorPosition.X;
+}
