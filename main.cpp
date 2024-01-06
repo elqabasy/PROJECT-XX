b9f5a0385656590970d5574caadeb12bd59fc397
Mahros <mahros.elqabasy@hotmail.com>
2024-01-07 00:48:39 +0200

    Feature-Animation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
-#include <chrono>
-#include <thread>
 #include <iostream>
-#include "headers/Console.h"
 using namespace std;
 
 
 int main(){ 
-    cout << "test" << endl;
-    cout << "Success!" << endl;
+    system("cls");
     return 0;
 }
